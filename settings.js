ca914b63c7e25036d272c672410668c33290f10e
Marco Antonio Caballero Moreno - TECA <31809509+markoAntonio1692@users.noreply.github.com>
2020-03-02 21:54:39 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -41,8 +41,11 @@ var settings = module.exports = {
     //httpAdminAuth: {user:"user",pass:"5f4dcc3b5aa765d61d8327deb882cf99"},
 
     // Serve up the welcome page
-    httpStatic: path.join(__dirname,"public"),
+    
+    //servir pagina estatica en la raiz
+    //httpStatic: path.join(__dirname,"public"),
 
+    
     functionGlobalContext: { },
 
     storageModule: require("./mongostorage"),
